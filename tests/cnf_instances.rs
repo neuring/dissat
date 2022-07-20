bd1b8b9f96d73a14148e9d4322ae61b533d86653
neuring <neuring@pm.me>
2022-07-20 16:30:32 +0200

    fix tests capturing logging output

--- tests/cnf_instances.rs
+++ tests/cnf_instances.rs
@@ -1,26 +1,50 @@
 #![feature(assert_matches)]
 #![feature(once_cell)]
+//#![feature(custom_test_frameworks)]
+//#![test_runner(datatest::runner)]
 
-use std::assert_matches::assert_matches;
+use std::{assert_matches::assert_matches, io::Write};
 
 use dissat::{Result, Solver};
+use tracing_subscriber::fmt::MakeWriter;
 
 fn set_up_log() {
     use std::sync::LazyLock;
     use tracing::metadata::LevelFilter;
-    use tracing_subscriber::fmt;
+
+    // hack: Create my own Writer that always uses print (more importantly print_to underneath)
+    // so that tests capture the logging output.
+    struct MyWriter;
+
+    impl Write for MyWriter {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            let s = std::str::from_utf8(buf).unwrap();
+            print!("{s}");
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            Ok(())
+        }
+    }
+
+    impl<'a> MakeWriter<'a> for MyWriter {
+        type Writer = Self;
+
+        fn make_writer(&'a self) -> Self::Writer {
+            MyWriter
+        }
+    }
 
     static LOG_SET_UP: LazyLock<()> = LazyLock::new(|| {
-        let fmt = fmt::format().without_time().compact();
-
         tracing_subscriber::fmt()
+            .with_writer(MyWriter)
             .with_max_level(LevelFilter::TRACE)
-            .event_format(fmt)
+            .without_time()
+            .compact()
             .init();
 
         tracing::info!("setup subscriber");
-
-        ()
     });
 
     LazyLock::force(&LOG_SET_UP);
@@ -35,7 +59,8 @@ fn decision_test() {
     ))
     .unwrap();
     let model = solver.solve().unwrap_sat();
-    assert!(model.lit(2))
+    assert!(model.lit(2));
+    print!("HI");
 }
 
 #[test]
